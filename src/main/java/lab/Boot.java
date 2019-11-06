eedc0f3b2270eea2070b4d4e7b1d0aa73f1e9083
dev <tonidev85@gmail.com>
2019-11-06 17:36:58 +0100

    Add Boot class

---
+++ src/main/java/lab/Boot.java
@@ -0,0 +1,11 @@
+package lab;
+
+/**
+ * Created by nmpacko on 06/11/2019.
+ */
+public class Boot {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
